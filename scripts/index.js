c1354407681eddf79e23aaf0919b169aaa188aea
ExGules <Respekt32113113@mail.ru>
2024-06-05 14:10:28 +0400

    END 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,18 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard(item) {
     const itemCard = cardTemplate.querySelector('.places__item').cloneNode(true);
     const deleteButton = itemCard.querySelector('.card__delete-button');
-    itemCard.querySelector('.card__image').src = item.link;
-    itemCard.querySelector('.card__image').alt = item.name;
+    const cardImage = itemCard.querySelector('.card__image');
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
     itemCard.querySelector('.card__title').textContent = item.name;
 
     // Передаем функцию deleteCard как колбэк
-    deleteButton.addEventListener('click', function(evt) {
-        deleteCard(evt);
-    });
+    deleteButton.addEventListener('click', deleteCard);
 
     return itemCard; 
 }
@@ -26,5 +25,5 @@ function deleteCard(evt) {
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(item) {
     const itemCard = createCard(item);
-    cardList.append(itemCard);
+    cardsContainer.append(itemCard);
 });
